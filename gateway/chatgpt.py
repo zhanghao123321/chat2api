dfde990ee7b7fdbf6e76a6d5b4be81b2839f9971
lanqian528 <5499636+lanqian528@users.noreply.github.com>
2025-03-05 14:46:18 +0800

    v1.8.3-beta1 fix language setup

--- gateway/chatgpt.py
+++ gateway/chatgpt.py
@@ -30,6 +30,8 @@ async def chatgpt_html(request: Request):
     user_chatgpt_context_2 = chatgpt_context_2.copy()
 
     set_value_for_key_list(user_chatgpt_context_1, "accessToken", token)
+    if request.cookies.get("oai-locale"):
+        set_value_for_key_list(user_chatgpt_context_1, "locale", request.cookies.get("oai-locale"))
 
     user_chatgpt_context_1 = json.dumps(user_chatgpt_context_1, separators=(',', ':'), ensure_ascii=False)
     user_chatgpt_context_2 = json.dumps(user_chatgpt_context_2, separators=(',', ':'), ensure_ascii=False)
