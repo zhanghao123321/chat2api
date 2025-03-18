c63d9308aaa60fdd5dad3984a6c7c3bc0e0e56b4
lanqian528 <5499636+lanqian528@users.noreply.github.com>
2025-03-18 12:39:56 +0800

    v1.8.4

--- gateway/v1.py
+++ gateway/v1.py
@@ -7,18 +7,21 @@ from app import app
 from gateway.reverseProxy import chatgpt_reverse_proxy
 from utils.kv_utils import set_value_for_key_dict
 
+with open("templates/initialize.json", "r") as f:
+    initialize_json = json.load(f)
+
 
 @app.post("/v1/initialize")
 async def initialize(request: Request):
-    initialize_response = (await chatgpt_reverse_proxy(request, f"v1/initialize"))
-    if not initialize_response:
-        return Response(status_code=204)
-    initialize_str = initialize_response.body.decode('utf-8')
-    if not initialize_str:
-        return Response(status_code=204)
-    initialize_json = json.loads(initialize_str)
-    set_value_for_key_dict(initialize_json, "ip", "8.8.8.8")
-    set_value_for_key_dict(initialize_json, "country", "US")
+    # initialize_response = (await chatgpt_reverse_proxy(request, f"v1/initialize"))
+    # if not initialize_response:
+    #     return Response(status_code=204)
+    # initialize_str = initialize_response.body.decode('utf-8')
+    # if not initialize_str:
+    #     return Response(status_code=204)
+    # initialize_json = json.loads(initialize_str)
+    # set_value_for_key_dict(initialize_json, "ip", "8.8.8.8")
+    # set_value_for_key_dict(initialize_json, "country", "US")
     return Response(content=json.dumps(initialize_json, indent=4), media_type="application/json")
 
 
